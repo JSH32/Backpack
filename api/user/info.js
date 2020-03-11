28d09244ad76c13810f8daddc312099b377f68b2
Riku32 <rudnik7000@gmail.com>
2020-03-11 18:58:45 +0000

    rename index to app

--- api/user/info.js
+++ api/user/info.js
@@ -6,22 +6,21 @@ module.exports = ({ db, app }) => {
         const { token } = req.body
         
         const Uploads = db.collection('uploads')
-            const Users = db.collection('users')
+        const Users = db.collection('users')
 
-            const tokenExists = Boolean(await Users.findOne({ token }))
+        const tokenExists = Boolean(await Users.findOne({ token }))
 
-            if (tokenExists) {
-                const { username } = await Users.findOne({ token })
+        if (tokenExists) {
+            const { username } = await Users.findOne({ token })
             
-                var filecount = await Uploads.countDocuments({ username })
+            var filecount = await Uploads.countDocuments({ username })
     
-                res.json({ 
-                    'username': username,
-                    'filecount': filecount
-                })
-            } else {
-                res.status(400).send('Invalid token!')
-            }
-        
+            res.json({ 
+                'username': username,
+                'filecount': filecount
+            })
+        } else {
+            res.status(400).send('Invalid token!')
+        }
     });
 }
