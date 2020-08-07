7e79e653bcb62952f9889ddd5bbf08c1adcef1eb
Deniernal354 <danielarston@gmail.com>
2020-08-07 18:30:41 +0300

    Allow sending of regkey if server is private
    The login function was modified to fetch the data from a global `dataObject` when initiating the post request. The object is then altered to include the `regkey` if the server was found to be in private mode.

--- public/signup/script.js
+++ public/signup/script.js
@@ -25,52 +25,30 @@ axios({
 })
 
 // I tried to shorten this as much as possible but i really couldnt :/
+
+/* Shortened version */
 function login () {
-  // Sending request with regkey if server is private mode
-  if (infoapi.inviteonly == true) {
+    // Declarations
     username = document.getElementById('userfield').value
     password = document.getElementById('passfield').value
     regkey = document.getElementById('regkeyfield').value
-
+  
+  const dataObject = {
+    username: username,
+    password: password
+  };
+  
+  // Sending request with regkey if server is private mode
+  if (infoapi.inviteonly === true) {
+    dataObject["regkey"] = regkey
+  }
+  
+    // Send default request without regkey
     axios({
       method: 'post',
       url: '/api/user/signup',
       data: {
-        username: username,
-        password: password,
-        regkey: regkey
-      }
-    }).then(function () {
-      Swal.fire({
-        title: 'Account created!',
-        text: 'Please log in now!',
-        icon: 'success',
-        showCancelButton: false,
-        confirmButtonColor: '#3085d6',
-        confirmButtonText: 'OK'
-      }).then((result) => {
-        if (result.value) {
-          window.location.replace('/login')
-        }
-      })
-    }).catch(function (error) {
-      if ($('#errortext').length > 0) {
-        $('#errortext').remove()
-      }
-      // Sending error text
-      $('#errormessage').append(`<div id="errortext" style="margin-top: 5px;"><p class="tag is-danger">${error.response.data}</p></div>`)
-    })
-  } else {
-    // Send request without regkey
-    username = document.getElementById('userfield').value
-    password = document.getElementById('passfield').value
-
-    axios({
-      method: 'post',
-      url: '/api/user/signup',
-      data: {
-        username: username,
-        password: password
+        dataObject
       }
     }).then(function () {
       Swal.fire({
@@ -93,4 +71,4 @@ function login () {
       $('#errormessage').append(`<div id="errortext" style="margin-top: 5px;"><p class="tag is-danger">${error.response.data}</p></div>`)
     })
   }
-}
+
