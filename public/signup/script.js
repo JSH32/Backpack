e65a3c41c8f2542ec3d86fedaf51204c3e970d66
Deniernal354 <danielarston@gmail.com>
2020-08-07 18:20:07 +0300

    Shorten the login function
    The login function was shortened to allow sending of the regkey if the server config is set to invite only.

--- public/signup/script.js
+++ public/signup/script.js
@@ -25,52 +25,31 @@ axios({
 })
 
 // I tried to shorten this as much as possible but i really couldnt :/
+
+/* SHORT VERSION */
+
 function login () {
-  // Sending request with regkey if server is private mode
-  if (infoapi.inviteonly == true) {
+  // Declare form variables
     username = document.getElementById('userfield').value
     password = document.getElementById('passfield').value
     regkey = document.getElementById('regkeyfield').value
-
+  
+  // Declare data object
+    const dataObj = {
+      username: username,
+      password: password
+    }
+ 
+  // Check if server is invite only
+  if (infoapi.inviteonly == true) {
+      dataObj.regkey = regkey // Add the regkey to the object
+  }
+    
     axios({
       method: 'post',
       url: '/api/user/signup',
       data: {
-        username: username,
-        password: password,
-        regkey: regkey
-      }
-    }).then(function () {
-      Swal.fire({
-        title: 'Account created!',
-        text: 'Please log in now!',
-        icon: 'success',
-        showCancelButton: false,
-        confirmButtonColor: '#3085d6',
-        confirmButtonText: 'OK'
-      }).then((result) => {
-        if (result.value) {
-          window.location.replace('/login')
-        }
-      })
-    }).catch(function (error) {
-      if ($('#errortext').length > 0) {
-        $('#errortext').remove()
-      }
-      // Sending error text
-      $('#errormessage').append(`<div id="errortext" style="margin-top: 5px;"><p class="tag is-danger">${error.response.data}</p></div>`)
-    })
-  } else {
-    // Send request without regkey
-    username = document.getElementById('userfield').value
-    password = document.getElementById('passfield').value
-
-    axios({
-      method: 'post',
-      url: '/api/user/signup',
-      data: {
-        username: username,
-        password: password
+        dataObj
       }
     }).then(function () {
       Swal.fire({
