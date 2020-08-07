3b0f22804896e2ebd2f6bd665cc280466be2d2dd
Restyled.io <commits@restyled.io>
2020-08-07 16:03:59 +0000

    Restyled by prettier

--- public/signup/script.js
+++ public/signup/script.js
@@ -4,16 +4,21 @@ let infoapi;
 // Checking if the token is valid
 if (localStorage.getItem("token") !== null) {
   axios({
-    method : "post",
-    url : "/api/token/valid",
-    data : {token : localStorage.getItem("token")},
+    method: "post",
+    url: "/api/token/valid",
+    data: { token: localStorage.getItem("token") },
   })
-      .then(function() { window.location.replace("/upload"); })
-      .catch(function() { localStorage.removeItem("token"); });
+    .then(function () {
+      window.location.replace("/upload");
+    })
+    .catch(function () {
+      localStorage.removeItem("token");
+    });
 }
 
-axios({method : "get", url : "/api/info"})
-    .then(function(response) { infoapi = response.data; });
+axios({ method: "get", url: "/api/info" }).then(function (response) {
+  infoapi = response.data;
+});
 
 // I tried to shorten this as much as possible but i really couldnt :/
 
@@ -24,7 +29,7 @@ function login() {
   password = document.getElementById("passfield").value;
   regkey = document.getElementById("regkeyfield").value;
 
-  const dataObject = {username, password};
+  const dataObject = { username, password };
 
   // Sending request with regkey if server is private mode
   if (infoapi.inviteonly === true) {
@@ -32,30 +37,30 @@ function login() {
   }
 
   // Send default request without regkey
-  axios({method : "post", url : "/api/user/signup", data : {dataObject}})
-      .then(function() {
-        Swal.fire({
-              title : "Account created!",
-              text : "Please log in now!",
-              icon : "success",
-              showCancelButton : false,
-              confirmButtonColor : "#3085d6",
-              confirmButtonText : "OK",
-            })
-            .then((result) => {
-              if (result.value) {
-                window.location.replace("/login");
-              }
-            });
-      })
-      .catch(function(error) {
-        if ($("#errortext").length > 0) {
-          $("#errortext").remove();
+  axios({ method: "post", url: "/api/user/signup", data: { dataObject } })
+    .then(function () {
+      Swal.fire({
+        title: "Account created!",
+        text: "Please log in now!",
+        icon: "success",
+        showCancelButton: false,
+        confirmButtonColor: "#3085d6",
+        confirmButtonText: "OK",
+      }).then((result) => {
+        if (result.value) {
+          window.location.replace("/login");
         }
-        // Sending error text
-        $("#errormessage")
-            .append(`<div id = "errortext" style = "margin-top: 5px;">
+      });
+    })
+    .catch(function (error) {
+      if ($("#errortext").length > 0) {
+        $("#errortext").remove();
+      }
+      // Sending error text
+      $(
+        "#errormessage"
+      ).append(`<div id = "errortext" style = "margin-top: 5px;">
                 <p class = "tag is-danger">
                     ${error.response.data}</ p></ div>`);
-      });
+    });
 }
