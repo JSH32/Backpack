948afbc028999bc421cbcef7c54bc713a34fdc5d
Riku32 <riku@kawaii.sh>
2021-03-25 01:27:26 +0000

    eslint and snowpack

--- frontend/snowpack.config.js
+++ frontend/snowpack.config.js
@@ -4,13 +4,13 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: { url: '/', static: true },
-    src: { url: '/dist' },
+    public: { url: "/", static: true },
+    src: { url: "/dist" },
   },
   plugins: [
-    '@snowpack/plugin-react-refresh',
-    '@snowpack/plugin-dotenv',
-    '@snowpack/plugin-typescript',
+    "@snowpack/plugin-react-refresh",
+    "@snowpack/plugin-dotenv",
+    "@snowpack/plugin-typescript",
   ],
   alias: {
     "components": "./src/components",
