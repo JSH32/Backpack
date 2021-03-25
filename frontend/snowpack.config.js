a2b967846f9773a9921316618fa9ff80c27127d7
Riku32 <riku@kawaii.sh>
2021-03-25 02:55:31 +0000

    declarations

--- frontend/snowpack.config.js
+++ frontend/snowpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     public: { url: "/", static: true },
     src: { url: "/dist" },
   },
+  "routes": [
+    {"match": "routes", "src": ".*", "dest": "/index.html"}
+  ],
   plugins: [
     "@snowpack/plugin-react-refresh",
     "@snowpack/plugin-dotenv",
---
+++ frontend/src/declarations.d.ts
@@ -0,0 +1,11 @@
+// Images
+declare module "*.png"
+declare module "*.jpg"
+declare module "*.svg"
+
+// Styles
+declare module "*.css"
+declare module "*.scss"
+
+// Other
+declare module "*.wasm"
