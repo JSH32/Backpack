2fad0f065c110e18e218e13393d9eb5c95936a1b
Riku32 <riku@kawaii.sh>
2021-04-01 08:04:03 +0000

    commented storageprovider

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -3,6 +3,7 @@ pub mod s3;
 use async_trait::async_trait;
 
 #[async_trait]
+/// Base storage provider type
 pub trait StorageProvider {
     /// Put the object/file on the storage source
     async fn put_object(&self, name: &str, data: Vec<u8>) -> Result<String, String>;
