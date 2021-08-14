645462cddf45c34e99b5b047cee52d89c00c5b7d
Riku32 <riku@kawaii.sh>
2021-08-14 05:26:09 -0500

    Cleanup Database
    Clean up some of the database stuff

--- src/database.rs
+++ src/database.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 use std::error::Error;
 
+use crate::models::UserData;
 use crate::models::{self, application::ApplicationData};
 
 use sqlx::migrate::Migrator;
@@ -20,6 +21,7 @@ impl Database {
                         .expect("Could not initialize connection")
         }
     }
+
     /// Run all pending up migrations
     pub async fn run_migrations(&self, path: &Path) -> Result<(), Box<dyn Error>> {
         let migrator = Migrator::new(path).await?;
@@ -27,19 +29,20 @@ impl Database {
 
         Ok(())
     }
+
     /// Creates a user from a user creation form
-    pub async fn create_user(&self, form: &models::user::UserCreateForm) -> Result<(), sqlx::Error> {
-        sqlx::query("INSERT INTO users (email, username, password) VALUES ($1, $2, $3)")
+    pub async fn create_user(&self, form: &models::user::UserCreateForm) -> Result<UserData, sqlx::Error> {
+        sqlx::query("INSERT INTO users (email, username, password) VALUES ($1, $2, $3) RETURNING *")
             .bind(&form.email)
             .bind(&form.username)
             .bind(&form.password)
-            .execute(&self.pool)
-            .await?;
-
-        Ok(())
+            .try_map(user_map)
+            .fetch_one(&self.pool)
+            .await
     }
+    
     /// Gets user info from database by email
-    pub async fn get_user_by_email(&self, email: &str) -> Result<models::user::UserData, sqlx::Error> {
+    pub async fn get_user_by_email(&self, email: &str) -> Result<UserData, sqlx::Error> {
         sqlx::query("SELECT id, email, username, password, verified, role FROM users WHERE email = $1")
             .bind(email)
             .try_map(user_map)
@@ -47,13 +50,14 @@ impl Database {
             .await
     }
     /// Gets user info from database by id
-    pub async fn get_user_by_id(&self, id: i32) -> Result<models::user::UserData, sqlx::Error> {
+    pub async fn get_user_by_id(&self, id: i32) -> Result<UserData, sqlx::Error> {
         sqlx::query("SELECT id, email, username, password, verified, role FROM users WHERE id = $1")
             .bind(id)
             .try_map(user_map)
             .fetch_one(&self.pool)
             .await
     }
+
     /// Gets user info from database by username
     pub async fn get_user_by_username(&self, username: &str) -> Result<models::user::UserData, sqlx::Error> {
         sqlx::query("SELECT id, email, username, password, verified, role FROM users WHERE username = $1")
@@ -62,6 +66,7 @@ impl Database {
             .fetch_one(&self.pool)
             .await
     }
+
     /// Delete a user and all their tokens
     pub async fn delete_user(&self, id: i32) -> Result<(), sqlx::Error> {
         sqlx::query("DELETE FROM users WHERE id = $1")
@@ -71,6 +76,7 @@ impl Database {
 
         Ok(())
     }
+
     /// Change a password for a user id
     pub async fn change_password(&self, id: i32, password: &str) -> Result<(), sqlx::Error> {
         sqlx::query("UPDATE users SET password = $1 WHERE id = $2")
@@ -81,6 +87,7 @@ impl Database {
 
         Ok(())
     }
+
     /// Delete an application by its id
     pub async fn delete_application_by_id(&self, application_id: i32) -> Result<(), sqlx::Error> {
         sqlx::query("DELETE FROM applications WHERE id = $1")
@@ -90,6 +97,7 @@ impl Database {
 
         Ok(())
     }
+
     /// Get an application by its id
     pub async fn get_application_by_id(&self, application_id: i32) -> Result<ApplicationData, sqlx::Error> {
         sqlx::query("SELECT id, name, user_id FROM applications WHERE id = $1")
@@ -98,6 +106,7 @@ impl Database {
             .fetch_one(&self.pool)
             .await
     }
+    
     /// Get all applications for a user from their id
     pub async fn get_all_applications(&self, user_id: i32) -> Result<Vec<ApplicationData>, sqlx::Error> {
         sqlx::query("SELECT id, name, user_id FROM applications WHERE user_id = $1")
@@ -106,6 +115,7 @@ impl Database {
             .fetch_all(&self.pool)
             .await
     }
+
     /// Create a new application
     pub async fn create_application(&self, user_id: i32, name: &str) -> Result<ApplicationData, sqlx::Error> {
         sqlx::query("INSERT INTO applications (user_id, name) VALUES ($1, $2) RETURNING id, user_id, name")
@@ -115,6 +125,7 @@ impl Database {
             .fetch_one(&self.pool)
             .await
     }
+
     /// Check if a token of that name already exists for a user
     pub async fn application_exist(&self, user_id: i32, name: &str) -> Result<bool, sqlx::Error> {
         let row: (bool,) = sqlx::query_as("SELECT EXISTS(SELECT 1 FROM applications WHERE name = $1 AND user_id = $2)")
@@ -125,6 +136,7 @@ impl Database {
 
         Ok(row.0)
     }
+
     /// Get the amount of applications a user has
     pub async fn application_count(&self, user_id: i32)-> Result<i64, sqlx::Error> {
         let row: (i64,) = sqlx::query_as("SELECT COUNT(*) FROM applications WHERE user_id = $1")
--- src/models/user.rs
+++ src/models/user.rs
@@ -22,6 +22,7 @@ pub struct PasswordChangeForm {
 /// User access level
 #[derive(Serialize, Deserialize, sqlx::Type, Eq, PartialEq, PartialOrd)]
 #[serde(rename_all(serialize  = "lowercase", deserialize  = "PascalCase"))]
+#[sqlx(type_name = "role")]
 pub enum UserRole {
     User,
     Admin
