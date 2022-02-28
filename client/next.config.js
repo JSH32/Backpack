0d6440b5fcb2ccf513c789251f0a1a510f6398ad
kakarot-dev <kakarot.joel@gmail.com>
2022-02-28 20:37:21 +0530

    proxy issues, im not good at this at all

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3001/api/:path*' // Proxy to Backend
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
