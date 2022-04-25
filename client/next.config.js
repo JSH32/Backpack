abb7937ade62a23fdfbff1c28df06d21729c91ef
JSH32 <rudnik7000@gmail.com>
2022-04-24 21:00:00 -0500

    Proxy files to base path from backend

--- client/next.config.js
+++ client/next.config.js
@@ -1,3 +1,6 @@
+// 0.0.0.0:3001 is the default 
+const BACKEND_URL = process.env.BACKEND_URL || "http://0.0.0.0:3001"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -5,7 +8,12 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.API_URL || "http://0.0.0.0:3001/api"}/:path*` // Proxy to Backend
+        destination: `${BACKEND_URL}/api/:path*` // Proxy to Backend
+      },
+      // Proxy the files from backpack API. Yes I know this will screw up 404 pages on non nested invalid URL's. Cry more, i'll fix later
+      {
+        source: "/:path",
+        destination: `${process.env.BACKEND_URL || "http://0.0.0.0:3001"}/:path` // Proxy to Backend
       }
     ]
   },
@@ -27,7 +35,7 @@ const nextConfig = {
     return config
   },
   env: {
-    API_URL: process.env.API_URL || "http://0.0.0.0:3001/api" // 0.0.0.0:3001 is the default
+    API_URL: `${BACKEND_URL}/api`
   }
 }
 
