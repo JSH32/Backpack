be8f2de58f136a77af643e24d4e654d7de6b6b14
JSH32 <rudnik7000@gmail.com>
2022-06-12 21:29:13 -0500

    NextJS development mode

--- client/next.config.js
+++ client/next.config.js
@@ -1,15 +1,32 @@
+// require('dotenv').config()
+// console.log("hio")
+// console.log(process.env.API_URL)
+
+// Use proxied URL in development
+const API_URL = process.env.NODE_ENV === "production"
+  ? process.env.API_URL
+  : `http://localhost:${process.env.PORT}`
+
+const rewrites = async () => {
+  const rewrites = [{
+    source: "/user/settings",
+    destination: "/user/settings/profile"
+  }]
+
+  // Proxy to Backend in development
+  if (process.env.NODE_ENV !== "production")
+    rewrites.push({
+      source: "/api/:path*",
+      destination: `${process.env.API_URL}/api/:path*`
+    })
+
+  return rewrites
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/user/settings",
-        destination: "/user/settings/profile",
-        permanent: true
-      }
-    ]
-  },
+  rewrites,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -19,11 +36,11 @@ const nextConfig = {
     return config
   },
   publicRuntimeConfig: {
-    apiUrl: `${process.env.API_URL}/api`
+    apiUrl: `${API_URL}/api`
   },
   serverRuntimeConfig: {
     // This url is used to fetch internal data on next server side
-    internalApiUrl: `${process.env.INTERNAL_API_URL || process.env.API_URL}/api`
+    internalApiUrl: `${process.env.INTERNAL_API_URL || API_URL}/api`
   }
 }
 
